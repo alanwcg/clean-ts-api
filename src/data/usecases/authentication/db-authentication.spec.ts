41c4292e7d65c82680c370d34564f4a5d8503e9b
alanwcg <alancintra7@gmail.com>
2022-10-08 10:37:00 -0300

    test: ensure DbAuthentication returns accessToken on success

--- src/data/usecases/authentication/db-authentication.spec.ts
+++ src/data/usecases/authentication/db-authentication.spec.ts
@@ -141,4 +141,10 @@ describe('DbAuthentication UseCase', () => {
     const promise = sut.auth(makeFakeAuthentication())
     await expect(promise).rejects.toThrow()
   })
+
+  it('should return accessToken if TokenGenerator succeeds', async () => {
+    const { sut } = makeSut()
+    const accessToken = await sut.auth(makeFakeAuthentication())
+    expect(accessToken).toBe('any_token')
+  })
 })
