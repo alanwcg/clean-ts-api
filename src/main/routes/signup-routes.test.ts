2eb274e9b04c7e94b2dec285ef898acf69d0fcbc
alanwcg <alancintra7@gmail.com>
2022-10-08 11:34:20 -0300

    test: ensure SignUp route returns 500 if unexpected error occurred

--- src/main/routes/signup-routes.test.ts
+++ src/main/routes/signup-routes.test.ts
@@ -3,6 +3,7 @@ import {
   MongoHelper,
   Collections
 } from '../../infra/db/mongodb/helpers/mongo-helper'
+import { DbAddAccount } from '../../data/usecases/add-account/db-add-account'
 import app from '../config/app'
 
 describe('SignUp Routes', () => {
@@ -19,6 +20,21 @@ describe('SignUp Routes', () => {
   beforeEach(async () => {
     const accountCollection = await mongoHelper.getCollection(Collections.ACCOUNTS)
     await accountCollection.deleteMany({})
+  })
+
+  it('should return 500 if unexpected error occurred', async () => {
+    jest.spyOn(DbAddAccount.prototype, 'add').mockImplementationOnce(() => {
+      throw new Error()
+    })
+    await request(app)
+      .post('/api/signup')
+      .send({
+        name: 'Alan Cintra',
+        email: 'alancintra7@gmail.com',
+        password: '123456',
+        passwordConfirmation: '123456'
+      })
+      .expect(500)
   })
 
   it('should return an account on success', async () => {
