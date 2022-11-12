24dd1eaa6c95dbfe6d1576928f3c919b3f0b5ebe
alanwcg <alancintra7@gmail.com>
2022-11-12 18:55:41 -0300

    feat: ensure AccountMongoRepository returns find an account by token without role

--- src/infra/db/mongodb/account/account-mongo-repository.ts
+++ src/infra/db/mongodb/account/account-mongo-repository.ts
@@ -5,11 +5,13 @@ import { AccountModel } from '../../../../domain/models/account'
 import { MongoHelper, Collections } from '../helpers/mongo-helper'
 import { LoadAccountByEmailRepository } from '../../../../data/protocols/db/account/load-account-by-email-repository'
 import { UpdateAccessTokenRepository, UpdateParams } from '../../../../data/protocols/db/account/update-access-token-repository'
+import { LoadAccountByTokenRepository } from '../../../../data/protocols/db/account/load-account-by-token-repository'
 
 export class AccountMongoRepository implements
   AddAccountRepository,
   LoadAccountByEmailRepository,
-  UpdateAccessTokenRepository {
+  UpdateAccessTokenRepository,
+  LoadAccountByTokenRepository {
   async add (accountData: AddAccountModel): Promise<AccountModel> {
     const mongoHelper = MongoHelper.getInstance()
     const accountCollection = await mongoHelper.getCollection(Collections.ACCOUNTS)
@@ -17,7 +19,7 @@ export class AccountMongoRepository implements
     return mongoHelper.mapper<AccountModel>(accountData)
   }
 
-  async loadByEmail (email: string): Promise<AccountModel> {
+  async loadByEmail (email: string): Promise<AccountModel | null> {
     const mongoHelper = MongoHelper.getInstance()
     const accountCollection = await mongoHelper.getCollection(Collections.ACCOUNTS)
     const account = await accountCollection.findOne<AccountModel>({ email })
@@ -34,5 +36,18 @@ export class AccountMongoRepository implements
       { _id: new ObjectId(id) },
       { $set: { accessToken } }
     )
+  }
+
+  async loadByToken (token: string, role?: string): Promise<AccountModel | null> {
+    const mongoHelper = MongoHelper.getInstance()
+    const accountCollection = await mongoHelper.getCollection(Collections.ACCOUNTS)
+    const account = await accountCollection.findOne<AccountModel>({
+      accessToken: token,
+      role
+    })
+    if (account) {
+      return mongoHelper.mapper(account)
+    }
+    return null
   }
 }
