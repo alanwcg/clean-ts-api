6ba68b477aa243f8d7e2a94224ac5264657a9af0
alanwcg <alancintra7@gmail.com>
2022-11-12 18:58:04 -0300

    test: ensure AccountMongoRepository finds an account by token with role

--- src/infra/db/mongodb/account/account-mongo-repository.spec.ts
+++ src/infra/db/mongodb/account/account-mongo-repository.spec.ts
@@ -90,5 +90,20 @@ describe('Account Mongo Repository', () => {
       expect(account?.email).toBe('any_email@mail.com')
       expect(account?.password).toBe('any_password')
     })
+
+    it('should return an account with role', async () => {
+      const sut = makeSut()
+      await accountCollection.insertOne({
+        ...makeFakeAccountData(),
+        accessToken: 'any_token',
+        role: 'any_role'
+      })
+      const account = await sut.loadByToken('any_token', 'any_role')
+      expect(account).toBeTruthy()
+      expect(account?.id).toBeTruthy()
+      expect(account?.name).toBe('any_name')
+      expect(account?.email).toBe('any_email@mail.com')
+      expect(account?.password).toBe('any_password')
+    })
   })
 })
