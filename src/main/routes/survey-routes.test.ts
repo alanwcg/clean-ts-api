1c6cd5848ce7483f848461c9c157094227446200
alanwcg <alancintra7@gmail.com>
2022-11-13 16:16:41 -0300

    test: ensure add survey route returns 400 with invalid body and 500 if unexpected error occured

--- src/main/routes/survey-routes.test.ts
+++ src/main/routes/survey-routes.test.ts
@@ -3,7 +3,7 @@ import { Collection } from 'mongodb'
 import { sign } from 'jsonwebtoken'
 import app from '../config/app'
 import env from '../config/env'
-// import { DbAddSurvey } from '../../data/usecases/add-survey/db-add-survey'
+import { DbAddSurvey } from '../../data/usecases/add-survey/db-add-survey'
 import { AddSurveyModel } from '../../domain/usecases/add-survey'
 import {
   MongoHelper,
@@ -25,35 +25,49 @@ const makeFakeSurveysRequestBody = (): AddSurveyModel => ({
 
 let surveyCollection: Collection
 let accountCollection: Collection
+let accessToken: string
 
 describe('Survey Routes', () => {
   const mongoHelper = MongoHelper.getInstance()
 
   beforeAll(async () => {
     await mongoHelper.connect(String(process.env.MONGO_URL))
+    accountCollection = await mongoHelper.getCollection(Collections.ACCOUNTS)
+    const account = await accountCollection.insertOne({
+      name: 'Alan Cintra',
+      email: 'alancintra7@gmail.com',
+      password: 'any_value',
+      role: 'admin'
+    })
+    const id = account.insertedId.toString()
+    accessToken = sign({ id }, env.jwtSecret)
+    await accountCollection.updateOne(
+      { _id: account.insertedId },
+      { $set: { accessToken } }
+    )
   })
 
   afterAll(async () => {
+    await accountCollection.deleteMany({})
     await mongoHelper.disconnect()
   })
 
   beforeEach(async () => {
     surveyCollection = await mongoHelper.getCollection(Collections.SURVEYS)
     await surveyCollection.deleteMany({})
-    accountCollection = await mongoHelper.getCollection(Collections.ACCOUNTS)
-    await accountCollection.deleteMany({})
   })
 
   describe('[POST] /surveys', () => {
-    // it('should return 400 if invalid body is provided', async () => {
-    //   await request(app)
-    //     .post('/api/surveys')
-    //     .send({})
-    //     .expect(400)
-    //     .expect(res => expect(res.body).toEqual({
-    //       error: expect.any(String)
-    //     }))
-    // })
+    it('should return 400 if invalid body is provided', async () => {
+      await request(app)
+        .post('/api/surveys')
+        .set('x-access-token', accessToken)
+        .send({})
+        .expect(400)
+        .expect(res => expect(res.body).toEqual({
+          error: expect.any(String)
+        }))
+    })
 
     it('should return 403 without accessToken', async () => {
       await request(app)
@@ -62,32 +76,21 @@ describe('Survey Routes', () => {
         .expect(403)
     })
 
-    // it('should return 500 if unexpected error occurred', async () => {
-    //   jest.spyOn(DbAddSurvey.prototype, 'add').mockImplementationOnce(() => {
-    //     throw new Error()
-    //   })
-    //   await request(app)
-    //     .post('/api/surveys')
-    //     .send(makeFakeSurveysRequestBody())
-    //     .expect(500)
-    //     .expect(res => expect(res.body).toEqual({
-    //       error: expect.any(String)
-    //     }))
-    // })
+    it('should return 500 if unexpected error occurred', async () => {
+      jest.spyOn(DbAddSurvey.prototype, 'add').mockImplementationOnce(() => {
+        throw new Error()
+      })
+      await request(app)
+        .post('/api/surveys')
+        .set('x-access-token', accessToken)
+        .send(makeFakeSurveysRequestBody())
+        .expect(500)
+        .expect(res => expect(res.body).toEqual({
+          error: expect.any(String)
+        }))
+    })
 
     it('should return 204 with valid token', async () => {
-      const account = await accountCollection.insertOne({
-        name: 'Alan Cintra',
-        email: 'alancintra7@gmail.com',
-        password: 'any_value',
-        role: 'admin'
-      })
-      const id = account.insertedId.toString()
-      const accessToken = sign({ id }, env.jwtSecret)
-      await accountCollection.updateOne(
-        { _id: account.insertedId },
-        { $set: { accessToken } }
-      )
       await request(app)
         .post('/api/surveys')
         .set('x-access-token', accessToken)
