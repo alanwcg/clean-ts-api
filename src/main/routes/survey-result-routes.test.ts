cdb1732e3c1eece45103079fc6703b8a3873199f
alanwcg <alancintra7@gmail.com>
2023-03-04 15:07:31 -0300

    test: ensure save survey result route return 200 on success

--- src/main/routes/survey-result-routes.test.ts
+++ src/main/routes/survey-result-routes.test.ts
@@ -1,9 +1,9 @@
 import request from 'supertest'
 import { Collection } from 'mongodb'
-// import { sign } from 'jsonwebtoken'
-// import env from '../config/env'
+import { sign } from 'jsonwebtoken'
+import env from '../config/env'
 import app from '../config/app'
-// import { AddSurveyModel } from '@/domain/usecases/survey/add-survey'
+import { AddSurveyModel } from '@/domain/usecases/survey/add-survey'
 // import { DbAddSurvey } from '@/data/usecases/survey/add-survey/db-add-survey'
 // import { DbLoadSurveys } from '@/data/usecases/survey/load-surveys/db-load-surveys'
 import {
@@ -11,45 +11,45 @@ import {
   Collections
 } from '@/infra/db/mongodb/helpers/mongo-helper'
 
-// const makeFakeSurveysRequestBody = (): Omit<AddSurveyModel, 'date'> => ({
-//   question: 'Question',
-//   answers: [
-//     {
-//       image: 'http://image-name.com',
-//       answer: 'Answer 1'
-//     },
-//     {
-//       answer: 'Answer 2'
-//     }
-//   ]
-// })
+const makeFakeSurveyData = (): AddSurveyModel => ({
+  question: 'Question',
+  answers: [
+    {
+      image: 'http://image-name.com',
+      answer: 'Answer 1'
+    },
+    {
+      answer: 'Answer 2'
+    }
+  ],
+  date: new Date()
+})
 
 let surveyCollection: Collection
-// let accountCollection: Collection
-// let accessToken: string
+let accountCollection: Collection
+let accessToken: string
 
 describe('Survey Routes', () => {
   const mongoHelper = MongoHelper.getInstance()
 
   beforeAll(async () => {
     await mongoHelper.connect(String(process.env.MONGO_URL))
-    // accountCollection = await mongoHelper.getCollection(Collections.ACCOUNTS)
-    // const account = await accountCollection.insertOne({
-    //   name: 'Alan Cintra',
-    //   email: 'alancintra7@gmail.com',
-    //   password: 'any_value',
-    //   role: 'admin'
-    // })
-    // const id = account.insertedId.toString()
-    // accessToken = sign({ id }, env.jwtSecret)
-    // await accountCollection.updateOne(
-    //   { _id: account.insertedId },
-    //   { $set: { accessToken } }
-    // )
+    accountCollection = await mongoHelper.getCollection(Collections.ACCOUNTS)
+    const account = await accountCollection.insertOne({
+      name: 'Alan Cintra',
+      email: 'alancintra7@gmail.com',
+      password: '123456'
+    })
+    const id = account.insertedId.toString()
+    accessToken = sign({ id }, env.jwtSecret)
+    await accountCollection.updateOne(
+      { _id: account.insertedId },
+      { $set: { accessToken } }
+    )
   })
 
   afterAll(async () => {
-    // await accountCollection.deleteMany({})
+    await accountCollection.deleteMany({})
     await mongoHelper.disconnect()
   })
 
@@ -67,5 +67,17 @@ describe('Survey Routes', () => {
         })
         .expect(403)
     })
+  })
+
+  it('should return 200 on save survey result with accessToken', async () => {
+    const res = await surveyCollection.insertOne(makeFakeSurveyData())
+    const surveyId = res.insertedId.toString()
+    await request(app)
+      .put(`/api/surveys/${surveyId}/results`)
+      .set('x-access-token', accessToken)
+      .send({
+        answer: 'Answer 1'
+      })
+      .expect(200)
   })
 })
