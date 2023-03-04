4180b2f55d804d0b174a943b574a34b48f6fc745
alanwcg <alancintra7@gmail.com>
2023-03-04 14:39:14 -0300

    test: ensure save survey result route return 403 if no token is provided

---
+++ src/main/routes/survey-result-routes.test.ts
@@ -0,0 +1,71 @@
+import request from 'supertest'
+import { Collection } from 'mongodb'
+// import { sign } from 'jsonwebtoken'
+// import env from '../config/env'
+import app from '../config/app'
+// import { AddSurveyModel } from '@/domain/usecases/survey/add-survey'
+// import { DbAddSurvey } from '@/data/usecases/survey/add-survey/db-add-survey'
+// import { DbLoadSurveys } from '@/data/usecases/survey/load-surveys/db-load-surveys'
+import {
+  MongoHelper,
+  Collections
+} from '@/infra/db/mongodb/helpers/mongo-helper'
+
+// const makeFakeSurveysRequestBody = (): Omit<AddSurveyModel, 'date'> => ({
+//   question: 'Question',
+//   answers: [
+//     {
+//       image: 'http://image-name.com',
+//       answer: 'Answer 1'
+//     },
+//     {
+//       answer: 'Answer 2'
+//     }
+//   ]
+// })
+
+let surveyCollection: Collection
+// let accountCollection: Collection
+// let accessToken: string
+
+describe('Survey Routes', () => {
+  const mongoHelper = MongoHelper.getInstance()
+
+  beforeAll(async () => {
+    await mongoHelper.connect(String(process.env.MONGO_URL))
+    // accountCollection = await mongoHelper.getCollection(Collections.ACCOUNTS)
+    // const account = await accountCollection.insertOne({
+    //   name: 'Alan Cintra',
+    //   email: 'alancintra7@gmail.com',
+    //   password: 'any_value',
+    //   role: 'admin'
+    // })
+    // const id = account.insertedId.toString()
+    // accessToken = sign({ id }, env.jwtSecret)
+    // await accountCollection.updateOne(
+    //   { _id: account.insertedId },
+    //   { $set: { accessToken } }
+    // )
+  })
+
+  afterAll(async () => {
+    // await accountCollection.deleteMany({})
+    await mongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    surveyCollection = await mongoHelper.getCollection(Collections.SURVEYS)
+    await surveyCollection.deleteMany({})
+  })
+
+  describe('[PUT] /surveys/:surveyId/results', () => {
+    it('should return 403 on save survey result without accessToken', async () => {
+      await request(app)
+        .put('/api/surveys/any_id/results')
+        .send({
+          answer: 'any_answer'
+        })
+        .expect(403)
+    })
+  })
+})
