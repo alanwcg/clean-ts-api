917886af213353a68c919246795e8b665c834407
alanwcg <alancintra7@gmail.com>
2023-03-10 14:13:49 -0300

    docs: update login and signup response schema

--- src/main/docs/schemas/account-schema.ts
+++ src/main/docs/schemas/account-schema.ts
@@ -4,7 +4,11 @@ export const accountSchema = {
     accessToken: {
       type: 'string',
       example: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzZmQ0MTFlNjQ4OTM2OGY4ZDc2NjM5ZCIsImlhdCI6MTY3Nzk1MjcyNn0.XcNWT1ujptUeUMs3h4csNdKB5sm2kuOfz8aU5TFf5Qo'
+    },
+    name: {
+      type: 'string',
+      example: 'Alan Cintra'
     }
   },
-  required: ['accessToken']
+  required: ['accessToken', 'name']
 }
