8fd9d67cce1af0210c7fa46d8ed393011f86ec40
alanwcg <alancintra7@gmail.com>
2023-03-17 17:49:55 -0300

    feat: add errors

--- src/main/adapters/apollo-server-resolver-adapter.ts
+++ src/main/adapters/apollo-server-resolver-adapter.ts
@@ -1,3 +1,5 @@
+import { GraphQLError } from 'graphql'
+import { ApolloServerErrorCode } from '@apollo/server/errors'
 import { Controller } from '@/presentation/protocols'
 
 export const adaptResolver = async (
@@ -5,5 +7,25 @@ export const adaptResolver = async (
   args: any
 ): Promise<any> => {
   const httpResponse = await controller.handle(args)
-  return httpResponse.body
+  switch (httpResponse.statusCode) {
+    case 200:
+    case 204:
+      return httpResponse.body
+    case 400:
+      throw new GraphQLError(httpResponse.body.message, {
+        extensions: { code: ApolloServerErrorCode.BAD_USER_INPUT }
+      })
+    case 401:
+      throw new GraphQLError(httpResponse.body.message, {
+        extensions: { code: 'AUTHENTICATION' }
+      })
+    case 403:
+      throw new GraphQLError(httpResponse.body.message, {
+        extensions: { code: 'FORBIDDEN' }
+      })
+    default:
+      throw new GraphQLError(httpResponse.body.message, {
+        extensions: { code: ApolloServerErrorCode.INTERNAL_SERVER_ERROR }
+      })
+  }
 }
